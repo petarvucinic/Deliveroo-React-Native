9b31f38698fc5a4ec67e80c2826674572fa8df21
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2023-01-03 19:03:17 +0100

    header and seacrhbar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,13 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Image } from "react-native";
+import {
+  AdjustmentsVerticalIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -14,19 +20,46 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="text-red-500">
-          <View>
-            <Image
-              source={{
-                url: "https://links.papareact.com/wru",
-              }}
-              className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="text-red-500">
+        {/* {header} */}
+
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+          <Image
+            source={{
+              url: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+
+        {/* {search} */}
+
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row space-x-2 bg-gray-200 p-3 flex-1">
+            <MagnifyingGlassIcon color="gray" size={20} />
+            <TextInput
+              placeholder="Restaurants and cuisines"
+              keyboardType="default"
             />
           </View>
-        </Text>
-      </View>
+          <AdjustmentsVerticalIcon color="#00CCBB" />
+        </View>
+
+            {/* {scrollview} */}
+
+
+      </Text>
     </SafeAreaView>
   );
 };
